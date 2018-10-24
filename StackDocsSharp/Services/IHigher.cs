5bae876b3a5bf4bedbde8fec30fe73ea96eee761
Tadas Ganitauskas <Tadas.Ganitauskas@gmail.com>
2018-10-24 20:37:05 +0300

    Added IHigher.cs

---
+++ StackDocsSharp/Services/IHigher.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+namespace StackDocsSharp.Services
+{
+    public class IHigher
+    {
+        public string readData()
+        {
+            CRUD readData = new CRUD();
+
+            //    List<string> listTopics = new List<string>();
+
+            DataTable dt = readData.Read("Topics");
+
+            string val = "";
+
+            if (dt.Rows.Count > 0)
+            {
+                val = dt.Rows[1]["Title"].ToString();
+            }
+
+            return val;
+        }
+    }
+}
