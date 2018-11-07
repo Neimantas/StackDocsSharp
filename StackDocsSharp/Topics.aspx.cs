b1e4e34c49da8e43029c930fb79551a275810c59
Edgaras Stroginis <e.stroginis@live.com>
2018-11-07 20:34:56 +0200

    [F19]Updated Topics page and code behind class

---
+++ StackDocsSharp/Topics.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using StackDocsSharp.Services;
+
+namespace StackDocsSharp
+{
+    public partial class Topics : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Topic.Text = e.ToString();
+            GetTopics();
+        }
+
+        public void GetTopics()
+        {
+            var readTopics = new CRUD();
+            var topicsData = readTopics.Read("Topics");
+            gwtopics.DataSource = topicsData;
+            gwtopics.DataBind();
+
+
+        }
+
+
+
+    }
+}
