be635cac5b6ab464a6eb046322b522ed1b7c5000
vytautas01 <vytautas.zvinys@stud.vgtu.lt>
2018-11-28 19:52:28 +0200

    [F13] fixed search logic

--- StackDocsSharp/Services/SearchEngine.cs
+++ StackDocsSharp/Services/SearchEngine.cs
@@ -3,7 +3,6 @@ using StackDocsSharp.Models.Const;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 
 namespace StackDocsSharp.Services
 {
@@ -13,13 +12,7 @@ namespace StackDocsSharp.Services
         {
             Higher _higher = new Higher();
             List<SearchResult> results = new List<SearchResult>();
-            List<CrudArgs> argsC = null;
-            foreach(string search in ParseSearchQuery(args))
-            {
-                CrudArgs arg = new CrudArgs("Contains(*, '"+search+"')", "", "");
-                argsC.Add(arg);
-            }
-            List<BLTopics> topicsList = _higher.GetTopics(argsC);
+            List<BLTopics> topicsList = _higher.GetTopics(ParseSearchQuery(args));
             foreach(BLTopics topic in topicsList)//search each topic and get the string with the text, title and id
             {
                 string description = GetDescription(topic.introductionPlain);
@@ -29,14 +22,20 @@ namespace StackDocsSharp.Services
             return results;
         }
 
-        public List<string> ParseSearchQuery(string text)
+        private List<CrudArgs> ParseSearchQuery(string text)
         {
-            var punctuation = text.Where(Char.IsPunctuation).Distinct().ToArray();
-            List<string> list = (List<string>) text.Split().Select(x => x.Trim(punctuation));
-            return list;
+            string[] arr = text.Split(' ');
+            List<CrudArgs> result = new List<CrudArgs>();
+            foreach (string word in arr)
+            {
+                if (word == " " || word == "") continue;
+                CrudArgs arg = new CrudArgs("*", "LIKE", "'%" + word + "%'");
+                result.Add(arg);
+            }
+            return result;
         }
 
-        public string GetDescription(string introduction)
+        private string GetDescription(string introduction)
         {
             if (introduction != null || introduction != "")
             {
