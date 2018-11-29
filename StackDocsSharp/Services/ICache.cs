0bff74364f0996f5c7248d0caa28ca3fc4e06d9d
Edgaras Stroginis <e.stroginis@live.com>
2018-11-29 15:15:27 +0200

    [F18] Created caching service

---
+++ StackDocsSharp/Services/ICache.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackDocsSharp.Services
+{
+    interface ICache
+    {
+       object GetObjectFromCache (string cachedItem);
+       void SetObjectToCache (string key, object item, int? cacheExpirationInMinutes = null);
+    }
+}
