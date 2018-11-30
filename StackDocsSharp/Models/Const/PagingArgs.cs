068a9238d17c24f04220ac24d9293dd1f3f09575
Edgaras Stroginis <e.stroginis@live.com>
2018-11-30 13:58:43 +0200

    [F9] Updated Paging.cs

---
+++ StackDocsSharp/Models/Const/PagingArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StackDocsSharp.Models.Const
+{
+    public class PagingArgs
+    {
+        private string _tableName;
+        private int _takeRows;
+        private int _skipRows;
+
+           
+        public string TableName { get; set; }
+        public int TakeRows { get; set; }
+        public int SkipRows { get; set; }
+    }
+}
+        
+        
+    
+
